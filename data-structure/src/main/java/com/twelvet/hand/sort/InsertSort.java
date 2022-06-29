6daa2f128d10d49c1dd983a45e61b238d61bc563
twelvet <2471835953@qq.com>
2022-06-29 23:05:33 +0800

    排序

--- data-structure/src/main/java/com/twelvet/hand/sort/InsertSort.java
+++ data-structure/src/main/java/com/twelvet/hand/sort/InsertSort.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 /**
  * @author twelvet
  * @WebSite www.twelvet.cn
- * @Description: 、插入排序
+ * @Description: 、插入排序-依次往前对比即可
  */
 public class InsertSort {
 
@@ -16,7 +16,22 @@ public class InsertSort {
     }
 
     public static void insertSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            // 定义带插入的数
+            int insertVal = arr[i];
+            // 即arr[1]的前面这个数的下标
+            int insertIndex = i - 1;
 
+            // insertIndex >= 0 保证insertVal 找到插入位置不越界
+            // insertVal < arr[insertIndex] 待插入的数，还没有找到插入位置
+            // 就需要arr[insertIndex]后移
+            while (insertIndex >= 0 && insertVal < arr[insertIndex]) {
+                arr[insertIndex + 1] = arr[insertIndex];
+                insertIndex--;
+            }
+            // 当退出while时说明插入位置找到，insertIndex - 1
+            arr[insertIndex + 1] = insertVal;
+        }
     }
 
 }
