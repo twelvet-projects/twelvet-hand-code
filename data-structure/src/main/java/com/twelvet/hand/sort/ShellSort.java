e2dd3153956754f68205912fa5fc6e6d81f51946
twelvet <2471835953@qq.com>
2022-06-30 23:24:29 +0800

    排序

---
+++ data-structure/src/main/java/com/twelvet/hand/sort/ShellSort.java
@@ -0,0 +1,61 @@
+package com.twelvet.hand.sort;
+
+import java.util.Arrays;
+
+/**
+ * @author twelvet
+ * @WebSite www.twelvet.cn
+ * @Description: 、希尔排序
+ */
+public class ShellSort {
+
+    public static void main(String[] args) {
+        int[] arr = {8, 9, 1, 7, 2, 3, 5, 4, 6, 0};
+        shellSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    /**
+     * 交换法
+     *
+     * @param arr
+     */
+    public static void shellSortReplace(int[] arr) {
+        int temp = 0;
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < arr.length; i++) {
+                for (int j = i - gap; j >= 0; j -= gap) {
+                    if (arr[j] > arr[j + gap]) {
+                        temp = arr[j];
+                        arr[j] = arr[j + gap];
+                        arr[j + gap] = temp;
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * 位移法
+     *
+     * @param arr
+     */
+    public static void shellSort(int[] arr) {
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+            // 从第gap个元素，逐个对其所在的组进行直接插入排序
+            for (int i = gap; i < arr.length; i++) {
+                int j = i;
+                int temp = arr[j];
+                if (arr[j] < arr[j - gap]) {
+                    while (j - gap >= 0 && temp < arr[j - gap]) {
+                        arr[j] = arr[j - gap];
+                        j -= gap;
+                    }
+                    arr[j] = temp;
+                }
+            }
+        }
+
+    }
+
+}
